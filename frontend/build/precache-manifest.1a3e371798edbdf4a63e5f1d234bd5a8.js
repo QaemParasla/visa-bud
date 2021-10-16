2ba9563d26c10ea2b341e5f8acec9a2515c2abd2
Qaem Parasla <qaemparasla@yahoo.com>
2021-10-16 18:43:44 -0500

    rebuild

---
+++ frontend/build/precache-manifest.1a3e371798edbdf4a63e5f1d234bd5a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20651dc929e30213afacb4932379074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa35b2444748177b65f",
+    "url": "/static/css/2.36743bfd.chunk.css"
+  },
+  {
+    "revision": "eb8bc3953af62041a2a1",
+    "url": "/static/css/main.a515c6da.chunk.css"
+  },
+  {
+    "revision": "aaa35b2444748177b65f",
+    "url": "/static/js/2.d6c393ae.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d6c393ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8bc3953af62041a2a1",
+    "url": "/static/js/main.462cf693.chunk.js"
+  },
+  {
+    "revision": "1dca37bd54c659d5608c",
+    "url": "/static/js/runtime-main.56b8db54.js"
+  },
+  {
+    "revision": "6bf66d967c6238298921ca38555549bd",
+    "url": "/static/media/contact-us.6bf66d96.jpg"
+  },
+  {
+    "revision": "0112d1538d7f71bd8b5488cf3f42bb41",
+    "url": "/static/media/visa_landing_image.0112d153.png"
+  }
+]);
